1a06e5d67a2653e50bfa9a5a2db8b2397830c6bd
Tony <tony@gmail.com>
2018-12-27 13:47:46 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -80,10 +80,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 }else if(currentLevel == LEVEL_COUNTY){
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);   // 跳转活动页面
-                    getActivity().finish();  // 关闭当前页面
+                    if(getActivity() instanceof MainActivity){        // 加入滑动菜单时，添加的内容
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);   // 跳转活动页面
+                        getActivity().finish();  // 关闭当前页面
+                    }else if(getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -4,10 +4,14 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -41,6 +45,12 @@ public class WeatherActivity extends AppCompatActivity {
 
     private ImageView bingPicImg;    // 必应背景图
 
+    public SwipeRefreshLayout swipeRefresh;   // 下拉刷新控件的声明
+    private String mWeatherId;
+
+    public DrawerLayout drawerLayout;    // 滑动菜单控件的声明
+    private Button navButton;   // 导航按钮
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,25 +76,46 @@ public class WeatherActivity extends AppCompatActivity {
 
         bingPicImg = (ImageView)findViewById(R.id.bing_pic_img);
 
+        swipeRefresh = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);  // 获取下拉刷新控件的实例
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);  // 设置下拉刷新进度条的颜色
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);   // 获取滑动菜单控件的实例
+        navButton = (Button)findViewById(R.id.nav_button);  // 获取导航按钮的实例
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);  // 共享参数存储 SharedPreferences
         String weatherString = prefs.getString("weather", null);
         String bingPic = prefs.getString("bing_pic", null);    // 处理必应图片
         if(weatherString != null){
             // 有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            mWeatherId = weather.basic.weatherId;  // 更新天气处理逻辑
             showWeatherInfo(weather);   // 调用下面自定义的方法   //////////////////////////////////////////
         }else {
             // 无缓存时去服务器查询天气
-            String weatherId = getIntent().getStringExtra("weather_id");
+            mWeatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);   ///////////////////////////////////////////////
-            requestWeather(weatherId);   // 调用下面自定义的方法   ////////////////////////////////////////
+            requestWeather(mWeatherId);   // 调用下面自定义的方法   ////////////////////////////////////////
         }
+
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {  // 注册下拉刷新控件监听器
+            @Override
+            public void onRefresh() {
+                requestWeather(mWeatherId);    // 刷新
+            }
+        });
 
         if(bingPic != null){     // 处理必应图片
             Glide.with(this).load(bingPic).into(bingPicImg);
         }else {
             loadBingPic();    // 调用下面自定义的方法，加载必应图片
         }
+
+        navButton.setOnClickListener(new View.OnClickListener() {   // 为导航按钮注册事件监听器
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 }
 
     /**
@@ -109,6 +140,7 @@ public class WeatherActivity extends AppCompatActivity {
                         }else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败！", Toast.LENGTH_SHORT).show();
                         }
+                        swipeRefresh.setRefreshing(false);  // 表示刷新事件结束，并隐藏刷新进度条
                     }
                 });
             }
